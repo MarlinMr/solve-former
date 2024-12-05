50260e069538b071d4766830a45d3917402c1c5d
MarlinMr <admin@marlinmr.se>
2024-12-06 00:11:32 +0100

    more functions

--- solve.py
+++ solve.py
@@ -1,6 +1,7 @@
 import random
 from colorama import Fore, Back, Style
 game={}
+e="K"
 remove=[]
 checked=[]
 legalMoves=[]
@@ -11,7 +12,7 @@ gameHeight=9
 low=gameWidth*gameHeight
 tested=0
 
-def printGame(game):
+def printGame():
     for i in range(gameHeight+2):
         for j in range(gameWidth+2):
             if [j,i] in remove:
@@ -32,7 +33,7 @@ def makeGame(game):
                 game[i][j]=random.choice(items)
     print(game)
     print(legalMoves)
-    printGame(game)
+    printGame()
 
 def checkGame(game,x,y):
     current=game[y][x]
@@ -61,6 +62,46 @@ def checkGame(game,x,y):
             remove.append([x+1,y])
         if ([x+1,y] not in checked):
             checkGame(game,x+1,y)
+
+def selVal(text,lim):
+    v=input(text)
+    if v=="q":
+        exit()
+    if v.isnumeric():
+        v=int(v)
+        if v in range(1,lim+1):
+            return v
+        else:
+            pass
+    else:
+        pass
+
+def calcTurn():
+    remove.clear()
+    x=selVal("x: ",gameWidth)
+    y=selVal("y: ",gameHeight)
+    move=[y,x]
+    #move=random.choice(legalMoves)
+    #x=move[0]
+    #y=move[1]
+    movesTaken.append(move)
+    remove.append([x,y])
+    e=game[y][x]
+    checkGame(game,x,y)
+    printGame()
+    for a in remove:
+        game[a[1]][a[0]]=" "
+    remove.clear()
+    for ii in range(gameHeight):
+        for i in range(1,gameHeight+1):
+            for j in range(1,gameWidth+1):
+                if game[i-1][j]==" ":
+                    game[i-1][j]=game[i][j]
+                    game[i][j]=" "
+    for a in legalMoves:
+        if game[a[1]][a[0]] == " ":
+            legalMoves.remove(a)
+
 while True:
     #makeGame(game)
     tested=tested+1
@@ -78,62 +119,12 @@ while True:
     9: {0: '.', 1: 'K', 2: 'O', 3: 'O', 4: 'K', 5: 'O', 6: 'N', 7: 'O', 8: '.'},
     10:{0: '.', 1: '.', 2: '.', 3: '.', 4: '.', 5: '.', 6: '.', 7: '.', 8: '.'}}
     legalMoves=[[1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [1, 4], [2, 4], [3, 4], [4, 4], [5, 4], [6, 4], [7, 4], [1, 5], [2, 5], [3, 5], [4, 5], [5, 5], [6, 5], [7, 5], [1, 6], [2, 6], [3, 6], [4, 6], [5, 6], [6, 6], [7, 6], [1, 7], [2, 7], [3, 7], [4, 7], [5, 7], [6, 7], [7, 7], [1, 8], [2, 8], [3, 8], [4, 8], [5, 8], [6, 8], [7, 8], [1, 9], [2, 9], [3, 9], [4, 9], [5, 9], [6, 9], [7, 9]]
-    #printGame(game)
-    while True:
-        remove.clear()
-        # x=input("x: ")
-        # if x=="q":
-            # break
-        # if x.isnumeric():
-            # x=int(x)
-        # else:
-            # continue
-        # if x not in range(1,gameWidth+1):
-            # continue
-        # y=input("y: ")
-        # if y=="q":
-            # break
-        # if y.isnumeric():
-            # y=int(y)
-        # else:
-            # continue
-        # if y not in range(1,gameHeight+1):
-            # continue
-        # if game[y][x] == " ":
-            # continue
-        move=random.choice(legalMoves)
-        movesTaken.append(move)
-        x=move[0]
-        y=move[1]
-        remove.append([x,y])
-        e=game[y][x]
-        #print(e)
-        #printGame(game)
-        checkGame(game,x,y)
-        #printGame(game)
-        for a in remove:
-            #print(game[a[1]][a[0]], end="")
-            game[a[1]][a[0]]=" "
-        #print()
-        remove.clear()
-        #printGame(game)
-        for ii in range(gameHeight):
-            for i in range(1,gameHeight+1):
-                for j in range(1,gameWidth+1):
-                    if game[i-1][j]==" ":
-                        #print("moving",i,j,game[i][j])
-                        game[i-1][j]=game[i][j]
-                        game[i][j]=" "
-        for a in legalMoves:
-            if game[a[1]][a[0]] == " ":
-                legalMoves.remove(a)
-        #print(legalMoves)
-        #printGame(game
-        if len(legalMoves)==0:
-            break
-    #print(movesTaken)
+    while len(movesTaken)<low and len(legalMoves)>0:
+        printGame()
+        calcTurn()
     print("Number of moves: ", len(movesTaken))
     if len(movesTaken)<low:
         low=len(movesTaken)
     print("Lowest: ", low)
     print("Tested :", tested)
+    print(movesTaken)
