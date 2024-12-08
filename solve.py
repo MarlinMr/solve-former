f68e891e771328d2d8b423d818dc4613c7cf1a58
MarlinMr <admin@marlinmr.se>
2024-12-08 19:45:56 +0100

    random from low and high

--- solve.py
+++ solve.py
@@ -7,33 +7,17 @@ game={}
 items=["K","O","N","D"]
 gameWidth=7
 gameHeight=9
-low=int(sys.argv[3])
+low=63
+limit=63
 tested=0
-limit=int(sys.argv[1])
-randomLevel=int(sys.argv[4])
+downLevel=int(sys.argv[1])
+topLevel=int(sys.argv[2])
 
 def printGame(currentGame):
     for i in reversed(range(gameHeight+2)):
         for j in range(gameWidth+2):
-            #if [j,i] in remove:
-            #    print(Fore.RED + game[i][j], end="")
-            #    print(Style.RESET_ALL, end="")
-            #else:
             print(currentGame[i][j], end="")
         print()
-
-def makeGame(game):
-    for i in range(gameHeight+2):
-        game[i]={}
-        for j in range(gameWidth+2):
-            if (((i==0) or (i==gameHeight+1))or((j==0)or(j==gameWidth+1))):
-                game[i][j]="."
-            else:
-                legalMoves.append([j,i])
-                game[i][j]=random.choice(items)
-    print(game)
-    print(legalMoves)
-    printGame()
 
 def checkGame(game,x,y,e,checked,remove):
     checked.append([x,y])
@@ -71,88 +55,17 @@ def selVal(text,lim):
     else:
         pass
 
-def calcTurn(game,movesTaken,low,tested,randomLevel,limit):
-    if sys.argv[2]=="random":
-        if (tested % limit)==0:
-            return(low,tested,randomLevel,limit)
-        tested=tested+1
-    localMoves=[]
-    currentGame=deepcopy(game)
-    if len(movesTaken)>=low:
-        print(f"\nBranch Terminated! oldLow: {low}, nMovesTaken: {len(movesTaken)} Tested: {tested} Limit: {limit} randomLevel: {randomLevel} MovesTaken: {movesTaken}", end="")
-
-        return(low,tested,randomLevel,limit)
-    for i in range(1,gameWidth+1):
-        for j in range(1,gameHeight+1):
-            if currentGame[j][i] in items:
-                localMoves.append([i,j])
-    if len(localMoves)<1:
-        print(f"\nBranch Ended! oldLow: {low}, nMovesTaken: {len(movesTaken)} Tested: {tested} Limit: {limit} randomLevel: {randomLevel} MovesTaken: {movesTaken}", end="")
-        if len(movesTaken)<low:
-            low=len(movesTaken)
-            f = open("results_r", "a")
-            result=f"Branch Ended! oldLow: {low}, nMovesTaken: {len(movesTaken)} Tested: {tested} Limit: {limit} randomLevel: {randomLevel} MovesTaken: {movesTaken}\n"
-            f.write(result)
-            f.close()
-            try:
-                with open("global_low", "r") as f:
-                    if low<int(f.read()):
-                        with open("global_low", "w") as ff:
-                            ff.write(str(low))
-            except:
-                pass
-            return(low,tested,randomLevel,limit)
-        return(low,tested,randomLevel,limit)
-    if len(sys.argv)>2:
-        if sys.argv[2]=="random" or len(movesTaken)>randomLevel-1:
-            random.shuffle(localMoves)
-    for move in localMoves:
-        if True:#sys.argv[2]!="random":
-            if (tested % limit)==0 and len(movesTaken)>randomLevel-1:
-                limit=int(limit/len(localMoves))
-                try:
-                    with open("global_low", "r") as f:
-                        k=int(f.read())
-                        if low>k:
-                            low=k
-                except Exception as e:
-                    pass
-                #print(f"break, {len(movesTaken)} {movesTaken}")
-                break#continue
-                #break
-            tested=tested+1
-        nextGame=deepcopy(currentGame)
-        x=move[0]
-        y=move[1]
-        e=nextGame[y][x]
-        movesTaken.append(move)
-        remove=[]
-        checked=[]
-        remove.append([x,y])
-        checkGame(nextGame,x,y,e,checked,remove)
-        for a in remove:
-            nextGame[a[1]][a[0]]="X"
-        for ii in range(gameHeight):
-            for i in range(1,gameHeight+1):
-                for j in range(1,gameWidth+1):
-                    if nextGame[i-1][j]=="X":
-                        nextGame[i-1][j]=nextGame[i][j]
-                        nextGame[i][j]="X"
-        low,tested,randomLevel,limit=calcTurn(nextGame,movesTaken,low,tested,randomLevel,limit)
-        movesTaken.pop()
-    return(low,tested,randomLevel,limit)
-
 game={
     10:{0: ' ', 1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: ''},
-    9: {0: '9 ', 1: 'D', 2: 'D', 3: 'O', 4: 'D', 5: 'K', 6: 'O', 7: 'K', 8: ''},
-    8: {0: '8 ', 1: 'N', 2: 'O', 3: 'O', 4: 'N', 5: 'O', 6: 'O', 7: 'K', 8: ''},
-    7: {0: '7 ', 1: 'O', 2: 'N', 3: 'O', 4: 'N', 5: 'K', 6: 'K', 7: 'N', 8: ''},
-    6: {0: '6 ', 1: 'D', 2: 'K', 3: 'K', 4: 'D', 5: 'O', 6: 'K', 7: 'O', 8: ''},
-    5: {0: '5 ', 1: 'D', 2: 'N', 3: 'D', 4: 'N', 5: 'D', 6: 'O', 7: 'N', 8: ''},
-    4: {0: '4 ', 1: 'D', 2: 'K', 3: 'N', 4: 'K', 5: 'D', 6: 'D', 7: 'N', 8: ''},
-    3: {0: '3 ', 1: 'O', 2: 'N', 3: 'O', 4: 'D', 5: 'O', 6: 'O', 7: 'N', 8: ''},
-    2: {0: '2 ', 1: 'K', 2: 'D', 3: 'K', 4: 'N', 5: 'O', 6: 'N', 7: 'D', 8: ''},
-    1: {0: '1 ', 1: 'N', 2: 'D', 3: 'O', 4: 'O', 5: 'O', 6: 'O', 7: 'K', 8: ''},
+    9: {0: '9 ', 1: 'D', 2: 'O', 3: 'D', 4: 'N', 5: 'O', 6: 'K', 7: 'K', 8: ''},
+    8: {0: '8 ', 1: 'D', 2: 'N', 3: 'D', 4: 'K', 5: 'D', 6: 'O', 7: 'N', 8: ''},
+    7: {0: '7 ', 1: 'N', 2: 'O', 3: 'D', 4: 'O', 5: 'O', 6: 'N', 7: 'K', 8: ''},
+    6: {0: '6 ', 1: 'K', 2: 'K', 3: 'D', 4: 'D', 5: 'K', 6: 'O', 7: 'D', 8: ''},
+    5: {0: '5 ', 1: 'D', 2: 'N', 3: 'D', 4: 'K', 5: 'O', 6: 'N', 7: 'D', 8: ''},
+    4: {0: '4 ', 1: 'K', 2: 'D', 3: 'K', 4: 'N', 5: 'D', 6: 'O', 7: 'N', 8: ''},
+    3: {0: '3 ', 1: 'K', 2: 'N', 3: 'O', 4: 'O', 5: 'N', 6: 'D', 7: 'K', 8: ''},
+    2: {0: '2 ', 1: 'N', 2: 'K', 3: 'O', 4: 'O', 5: 'O', 6: 'K', 7: 'K', 8: ''},
+    1: {0: '1 ', 1: 'K', 2: 'N', 3: 'O', 4: 'O', 5: 'D', 6: 'K', 7: 'D', 8: ''},
     0: {0: '  ', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: ''}}
 #game={
 #    5:{0: ' ', 1: '', 2: '', 3: '', 4: '', 5: ''},
@@ -163,14 +76,61 @@ game={
 #    0: {0: '  ', 1: '1', 2: '2', 3: '3', 4: '4', 5: ''}}
 #legalMoves=[[1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [1, 4], [2, 4], [3, 4], [4, 4], [5, 4], [6, 4], [7, 4], [1, 5], [2, 5], [3, 5], [4, 5], [5, 5], [6, 5], [7, 5], [1, 6], [2, 6], [3, 6], [4, 6], [5, 6], [6, 6], [7, 6], [1, 7], [2, 7], [3, 7], [4, 7], [5, 7], [6, 7], [7, 7], [1, 8], [2, 8], [3, 8], [4, 8], [5, 8], [6, 8], [7, 8], [1, 9], [2, 9], [3, 9], [4, 9], [5, 9], [6, 9], [7, 9]]
 movesTaken=[]
+
+def testGame(game,movesTaken,low,tested):
+    currentGame=deepcopy(game)
+    localMoves=[]
+    for i in range(1,gameWidth+1):
+        for j in range(1,gameHeight+1):
+            if currentGame[j][i] in items:
+                localMoves.append([i,j])
+    if len(localMoves)==0:
+        print(f"DONE! Found {len(movesTaken)} after {tested} branches. {movesTaken}")
+        if low>len(movesTaken):
+            low=len(movesTaken)
+        return(low,tested)
+    #print(f"{len(localMoves)} localMoves: {localMoves}")
+    #nMoves=nMoves*len(localMoves)
+    if len(movesTaken)>=topLevel:
+        random.shuffle(localMoves)
+    for move in localMoves:
+        #print(f"Breaking: {tested} low: {low}, {len(movesTaken)} movesTaken: {movesTaken}")
+        movesTaken.append(move)
+        if ((tested % downLevel) == 0) and (len(movesTaken)>topLevel):
+            movesTaken.pop()
+            #print(f"Breaking: {tested} low: {low}, {len(movesTaken)} movesTaken: {movesTaken}")
+            break
+        tested=tested+1
+        if len(movesTaken)>=low:
+            print(f"Continue: {tested} low: {low}, {len(movesTaken)} movesTaken: {movesTaken}")
+            movesTaken.pop()
+            break
+        nextGame=deepcopy(currentGame)
+        e=nextGame[move[1]][move[0]]
+        checked=[]
+        remove=[]
+        remove.append(move)
+        checkGame(nextGame,move[0],move[1],e,checked,remove)
+        for a in remove:
+            nextGame[a[1]][a[0]]="X"
+        for ii in range(gameHeight):
+            for i in range(1,gameHeight+1):
+                for j in range(1,gameWidth+1):
+                    if nextGame[i-1][j]=="X":
+                        nextGame[i-1][j]=nextGame[i][j]
+                        nextGame[i][j]="X"
+        low,tested=testGame(nextGame,movesTaken,low,tested)
+        movesTaken.pop()
+    #print(f"Tested: {tested} low: {low}, {len(movesTaken)} movesTaken: {movesTaken}")
+    return(low,tested)
+
+try:
+    with open("global_low", "r") as f:
+        k=int(f.read())
+        if low>k:
+            low=k
+except Exception as e:
+    pass
+nMoves=1
 while True:
-    tested=tested+1
-    try:
-        with open("global_low", "r") as f:
-            k=int(f.read())
-            if low>k:
-                low=k
-    except Exception as e:
-        pass
-    low,tested,randomLevel,limit=calcTurn(game,movesTaken,low,tested,randomLevel,limit)
-    randomLevel=randomLevel+1
+    low,tested=testGame(game,movesTaken,low,tested)
